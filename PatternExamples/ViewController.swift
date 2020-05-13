4dba5a632a2f8ae074384666880e5d72eb1bdb3c
skinnerrula <skinklass@mail.ru>
2020-05-13 07:55:22 +0300

    Initial Commit

---
+++ PatternExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PatternExamples
+//
+//  Created by Дмитрий Федоринов on 13.05.2020.
+//  Copyright © 2020 Дмитрий Федоринов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
